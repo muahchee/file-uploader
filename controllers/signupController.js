cd4b7230ca9c9239d78557fdefd0cb4eae04ff3c
muahchee <187490843+muahchee@users.noreply.github.com>
2025-12-24 17:23:19 +1300

    build: signup

---
+++ controllers/signupController.js
@@ -0,0 +1,69 @@
+import {
+  createUser,
+  getUserByEmail,
+  getUserByUsername,
+} from "../lib/queries.js";
+
+import { body, validationResult, matchedData } from "express-validator";
+import bcrypt from "bcryptjs";
+
+const validateSignup = [
+  body("username")
+    .trim()
+    .custom(async (value) => {
+      const user = await getUserByUsername(value);
+      if (user) throw new Error("Username already in use");
+    }),
+  body("email")
+    .trim()
+    .isEmail()
+    .optional({ values: falsy })
+    .withMessage(
+      "Email is not in a valid format. Expected format is 'user@email.com'"
+    )
+    .custom(async (value) => {
+      const user = await getUserByEmail(value);
+      if (user) throw new Error("Email already in use");
+    }),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password should be at least 5 characters long"),
+  body("confirm-password").custom(async (value, { req }) => {
+    if (value != req.body.password) throw new Error("Passwords don't match!");
+  }),
+];
+
+export async function addUserGet(req, res) {
+  res.render("signup");
+}
+
+export const addUserPost = [
+  validateSignup,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("signup", {
+        errors: errors.array(),
+        obj: {
+          username: req.body.username,
+          email: req.body.email ? req.body.email : null,
+        },
+      });
+    }
+    const { username, password, email } = matchedData(req, {
+      includeOptionals: true,
+    });
+
+    try {
+      const hashedPw = await bcrypt.hash(password, 10);
+      await createUser({
+        username: username,
+        email: email,
+        password: hashedPw,
+      });
+      res.redirect("/");
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { initialFoldersGet } from "../controllers/indexController.js";
+import { addUserGet, addUserPost } from "../controllers/signupController.js";
 
-export const indexRouter = Router()
+export const indexRouter = Router();
 
-indexRouter.get("/", initialFoldersGet)
-indexRouter.post("/signup", )
+indexRouter.get("/", initialFoldersGet);
+indexRouter.get("/signup", addUserGet);
+indexRouter.post("/signup", addUserPost);
